369ddc32d7d2a0e7d659b23505cb39a87f49837d
Rohit Ranjan <rohitranjan1612@gmail.com>
2025-12-31 16:36:52 +0530

    api changes

--- lib/api.ts
+++ lib/api.ts
@@ -1,34 +1,28 @@
-import { Product } from '@/data/products';
+import { Product } from "@/data/products";
 
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-
-export async function fetchProducts(): Promise<Product[]> {
-  const res = await fetch(`${API_BASE_URL}/api/products`, {
-    // cache: 'force-cache', // SSG
+export async function fetchProducts() {
+  const res = await fetch("/api/products", {
+    cache: "force-cache",
   });
 
   if (!res.ok) {
-    throw new Error('Failed to fetch products');
+    throw new Error("Failed to fetch products");
   }
 
   return res.json();
 }
 
-export async function fetchProductBySlug(
-  slug: string
-): Promise<Product> {
-  const res = await fetch(
-    `${API_BASE_URL}/api/products?slug=${slug}`,
-    // { cache: 'force-cache' } // SSG
-  );
+export async function fetchProductBySlug(slug: string) {
+  const res = await fetch(`/api/products?slug=${slug}`, {
+    cache: "force-cache",
+  });
 
   if (res.status === 404) {
-    throw new Error('NOT_FOUND');
+    throw new Error("NOT_FOUND");
   }
 
   if (!res.ok) {
-    throw new Error('Failed to fetch product');
+    throw new Error("Failed to fetch product");
   }
 
   return res.json();
